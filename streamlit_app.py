4682812affcb234d3aa7de22d92ba1c680a10d4f
kwonsu84 <151598566+kwonsu84@users.noreply.github.com>
2024-02-09 20:49:06 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,23 +7,20 @@ st.set_page_config(
     layout="centered"
 )
 
-
-st.subheader(":robot_face: :blue[K-에듀파인 업무관리 AI챗봇]")
-
-with st.chat_message("assistant"):
-    st.write("👋 K-에듀파인 업무관리에 대해서 궁금한 내용을 모두 입력해주세요. 따라하기 메뉴얼을 기반으로 답변을 전달합니다.")
-
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo-1106"
-
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-
-
 with st.container():
+    st.subheader(":robot_face: :blue[K-에듀파인 업무관리 AI챗봇]")
+    
+    with st.chat_message("assistant"):
+        st.write("👋 K-에듀파인 업무관리에 대해서 궁금한 내용을 모두 입력해주세요. 따라하기 메뉴얼을 기반으로 답변을 전달합니다.")
+    
+    client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+    
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-3.5-turbo-1106"
+    
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
