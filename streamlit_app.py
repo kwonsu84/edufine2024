4fee2d1ab6330b6c9f5baf739787e8a573f7a56c
kwonsu84 <151598566+kwonsu84@users.noreply.github.com>
2024-02-09 20:48:24 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,11 +21,13 @@ if "openai_model" not in st.session_state:
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+
 
 with st.container():
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+        
     if prompt := st.chat_input("궁금한 내용을 입력하세요!"):
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
