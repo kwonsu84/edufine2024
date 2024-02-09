bbcf2066b6d4a30882f5a8addb627ff8d44f2fb7
kwonsu84 <151598566+kwonsu84@users.noreply.github.com>
2024-02-09 15:07:08 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
 
 for message in st.session_state.messages:
-    if "user" = st.chat_message(message["role"]):
+    if "user" == st.chat_message(message["role"]):
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
