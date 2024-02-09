d00f96c27222d34b19a33a672531a2df703a07f8
kwonsu84 <151598566+kwonsu84@users.noreply.github.com>
2024-02-09 14:32:38 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 from openai import OpenAI
 import streamlit as st
 
-
+st.set_page_config(
+    page_title="검증된 주식관련 데이터로 학습한 AI 챗봇",
+    layout="wide",
+    initial_sidebar_state="auto",
+    menu_items=None
+)
 st.subheader(":robot_face: K-에듀파인 업무관리")
 st.caption(':grinning: _따라하기 메뉴얼을 학습하여 답변의 정확성을 높였습니다._')
 
