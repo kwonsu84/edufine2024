cb99cf59364aeea3741f9ff07986b11382a9c3ee
kwonsu84 <151598566+kwonsu84@users.noreply.github.com>
2024-02-09 15:40:19 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,9 +32,9 @@ if prompt := st.chat_input("궁금한 내용을 입력하세요!"):
     with st.chat_message("assistant"):
         #RAG형식으로 구현!
         messages = [
-        {"role": "system", "content": "당신은 챗봇입니다."}
+        {"role": "system", "content": "당신은 챗봇입니다. 관련자료에 기반해서만 답변합니다."}
         ] + st.session_state.messages + [
-        {"role": "user", "content": "대한민국 교육부와 그 예하 준정부기관인 한국교육학술정보원의 소관 하에 운영되고 있는 국가관리회계시스템. 명칭 중 Edu는 교육(Education)을, Fine은 재정(Finance)을 뜻한다. 주소는 각 시·도교육청 주소 앞에 klef.을 붙이면 된다. 해당 교육청 내부망에서만 접속할 수 있다."}
+        {"role": "user", "content": "관련자료 : 대한민국 교육부와 그 예하 준정부기관인 한국교육학술정보원의 소관 하에 운영되고 있는 국가관리회계시스템. 명칭 중 Edu는 교육(Education)을, Fine은 재정(Finance)을 뜻한다. 주소는 각 시·도교육청 주소 앞에 klef.을 붙이면 된다. 해당 교육청 내부망에서만 접속할 수 있다."}
         ]
         
         stream = client.chat.completions.create(
