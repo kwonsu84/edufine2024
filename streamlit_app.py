d449d1231bc6250fa0a0a1c1a3d6abfa9d1bfa40
kwonsu84 <151598566+kwonsu84@users.noreply.github.com>
2024-02-09 14:28:19 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,6 @@ import streamlit as st
 st.subheader(":robot_face: K-에듀파인 업무관리")
 st.caption(':grinning: _따라하기 메뉴얼을 학습하여 답변의 정확성을 높였습니다._')
 
-st.set_page_config(
-    page_title="K-에듀파인 업무관리",
-    layout="wide"
-)
 
 with st.chat_message("assistant"):
     st.write("👋 안녕하세요. K-에듀파인 업무관리에 대해서 궁금한 내용을 모두 입력해주세요.")
