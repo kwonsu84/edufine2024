4b993cfe93f2ee9b9a4214b93c8117ab167942b2
kwonsu84 <151598566+kwonsu84@users.noreply.github.com>
2024-02-20 12:42:29 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,10 +55,9 @@ with st.container(border=True):
             #RAG형식으로 구현!
             messages = [
             {"role": "system", "content": "너는 한국교육학술정보원의 'K-에듀파인 업무관리 시스템' 챗봇이다. 너는 '2024년 기준으로 제작된 관련자료(따라하기 메뉴얼, 교육동영상 등)'를 제공받는다. 하지만 '관련자료'는 너에게만 제공하고 질문자는 볼 수 있다. 질문자들은 'K-에듀파인 업무관리 시스템'의 사용법에 대해서 질문한다."},
-            {"role": "system", "content": "너의 답변은 핵심 위주로 간결하고, 가독성 높은 글을 작성한다. 시스템 사용법 문의와 관련없는 질문에 대해서는 답변하지 않는다. 답변의 마지막에 '출처'와 '출처 내 위치'를 포함한다. '출처: , 위치: ' 형식으로 참고한 자료를 알려준다."}
-            ] + st.session_state.messages + [
+            {"role": "system", "content": "너의 답변은 핵심 위주로 간결하고, 가독성 높은 글을 작성한다. 시스템 사용법 문의와 관련없는 질문에 대해서는 답변하지 않는다. 답변의 마지막에 '출처'와 '출처 내 위치'를 포함한다. '출처: , 위치: ' 형식으로 참고한 자료를 알려준다."},
             {"role": "system", "content": get_db_data(prompt)}
-            ]
+            ] + st.session_state.messages
             
             stream = client.chat.completions.create(
                 model=st.session_state["openai_model"],
